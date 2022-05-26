49750f0034ab671317752423cf73153b67c48ff7
knutaf <knutaf@gmail.com>
2022-05-25 22:21:06 -0700

    fix merge to public

--- merge_to_public.cmd
+++ merge_to_public.cmd
@@ -10,6 +10,6 @@ git checkout public
 if %errorlevel% NEQ 0 (
     goto :EOF
 )
-cmd /c gitm master
+cmd /c gitm main
 git rm -f %repo%\merge_to_public.cmd
 REM add more git rm -f lines here to remove other private files
